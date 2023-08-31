3dd063aef73c97930e324b817b81b9f5500493d2
Luis Fernando Farji <fernando.farji@gmail.com>
2023-08-31 18:48:19 -0300

    javascript

---
+++ js/music.js
@@ -0,0 +1,57 @@
+  const playlist= [
+      {
+          songName : "alan-walker-alone",
+          songImg : "/images/alanWalker.jpg",
+          songSrc : "/music/alan-walker-alone.mp3"
+      },
+      {
+          songName : "apna-time-aayega-gully-boy",
+          songImg : "/images/apnaTime.jpg",
+          songSrc : "/music/apna-time-aayega-gully-boy.mp3"
+      },
+      {
+          songName : "Sanu Ek Pal Chain - Raid",
+          songImg : "/images/sanuEkPal.jpg",
+          songSrc : "/music/Sanu Ek Pal Chain - Raid.mp3"
+      },
+      {
+          songName : "jitni-dafa-dekhu-tujhe",
+          songImg : "/images/jitniDafa.png",
+          songSrc : "/music/jitni-dafa-dekhu-tujhe.mp3"
+      },
+      {
+          songName : "love-me-like-you-do",
+          songImg : "/images/Love Me Like You Do.png",
+          songSrc : "/music/love-me-like-you-do.mp3"
+      },
+      {
+          songName : "Post Malone - Hollywood s Bleeding (Audio)",
+          songImg : "/images/hollywood.jpg",
+          songSrc : "/music/Post Malone - Hollywood s Bleeding (Audio).mp3"
+      },
+      {
+          songName : "roi-na-je-yaad-meri-aayi-ve",
+          songImg : "/images/royiNa.jpg",
+          songSrc : "/music/roi-na-je-yaad-meri-aayi-ve.mp3"
+      },
+      {
+          songName : "The Heart Wants What It Wants (Official Video) (1)",
+          songImg : "/images/selena-gomez1.jpg",
+          songSrc : "/music/Selena Gomez - The Heart Wants What It Wants (Official Video) (1).mp3"
+      },
+      {
+          songName : "tere-jaisa-yaar-kahan-new-version",
+          songImg : "/images/tereJaisa.jpg",
+          songSrc : "/music/tere-jaisa-yaar-kahan-new-version.mp3"
+      },
+      {
+          songName : "Bon Appetit",
+          songImg : "/images/bonApetit.jpg",
+          songSrc : "/music/Bon Appetit.mp3"
+      }
+  ]
+
+  var handle = document.getElementById('music');
+  var songName = document.getElementById('song-name');
+  var songImg = document.getElementById('song-img');
+  var play = document.getElementById('play');
