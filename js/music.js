570ba4abdcf8ecd792bd118f96a432866fb2838f
Luis Fernando Farji <fernando.farji@gmail.com>
2023-09-08 10:57:26 -0300

    music player

--- js/music.js
+++ js/music.js
@@ -55,3 +55,129 @@
   var songName = document.getElementById('song-name');
   var songImg = document.getElementById('song-img');
   var play = document.getElementById('play');
+  var pause= document.getElementById('pause');
+  handle.controls = false;
+
+  function togglePlayList() {
+    document.getElementById('playlist').classList.toggle("togglePlaylist");
+  }
+  function togglePlay(){
+    if (handle.paused || handle.ended) {
+        play.style.display="none";
+        pause.style.display='inline-block';
+        handle.play();
+    }
+    else {
+        pause.style.display='none';
+        play.style.display='inline-block';
+        handle.pause();
+    }
+  }
+
+
+  function setVolume() {
+    var volume = document.getElementById('volume');
+    handle.volume = volume.value;
+  }
+
+  function loop(){
+    handle.loop = !handle.loop;
+    if(handle.loop){
+        document.getElementById('repeat').classList.add('repeat-active');
+    }
+    else {
+        document.getElementById('repeat').classList.remove('repeat-active');
+    }
+  }
+  function toggleMute(){
+    if (!handle.muted) {
+        document.getElementById('volume').value="0";
+    }
+    else {
+        document.getElementById('volume');
+        value.handle.volume;
+    }
+    handle.muted = !handle.muted;
+  }
+  var duration = 0;
+  handle.onloadedmetadata = function(){
+    duration = Math.floor(handle.duration);
+  }
+
+
+
+
+  function update (){
+    var progress = document.getElementById('progress');
+    if(handle.currentTime>0){
+        progress.value = Math.floor((100/handle.duration)*handle.currentTime)
+    }
+    var minDuration = Math.floor(duration/60);
+    var secDuration = duration - minDuration*60;
+    document.getElementById('song-duration').innerHTML = minDuration + ":" + secDuration;
+    var time = Math.floor(handle.currentTime);
+    var min = Math.floor(time/60);
+    var seconds = time - time * 60;
+    if (seconds<10){
+        document.getElementById ('current-time').innerHTML = min+ ":0"
+        + seconds;
+    }
+    else {
+        document.getElementById('current-time').innerHTML= min+ ":"+ seconds;
+    }
+  }
+  var i=0;
+  function next(){
+    var prev="id" + i;
+    document.getElementById(prev).classList.remove("addClass")
+    if(i<playlist.length-1){
+        i=i+1;
+    }
+    else{
+        i=0;
+    }
+    songImg.src= playlist[i].songImg;
+    songName.innerHTML = playlist[i].songName;
+    handle.src = playlist[i].songSrc;
+    handle.autoplay = "true";
+    var current = "id" + i;
+    document.getElementById(current).classList.add("addClass");
+    document.getElementById("song-duration").innerHTML="0:0"
+    togglePlay();
+}
+
+function back(){
+    var prev = "id"+i;
+    document.getElementById(prev).classList.remove("addClass");
+    if (i>0){
+        i= i-1;
+    }
+    songImg.src = playlist[i].songImg;
+    songName.innerHTML = playlist[i].songName;
+    handle.src=playlist[i].songSrc;
+    handle.autoplay=true;
+    var current = "id"+i;
+    document.getElementById(current).classList.add("addClass");
+    document.getElementById("song-duration").innerHTML = "0:0";
+    togglePlay();
+}
+
+function songByChoice(){
+    var prev= "id" + i;
+    document.getElementById(prev).classList.remove ("addClass");
+    var get="/music/"+document.getElementById(e.id).innerHTML + ".mp3";
+    i= playlist.findIndex(x=> x.songSrc===get);
+    songImg.src = playlist[i].songImg;
+    songName.innerHTML = playlist[i].songName;
+    handle.src = playlist[i].songSrc;
+    var current = "id" + i;
+    document.getElementById(current).classList.add("addClass");
+    handle.autoplay = "true";
+    togglePlay() ;  //to play the music after choosing it from dropdown menu
+}
+  var disableUpdate = false;
+  var progress = document.getElementById('progress');
+  progress.addEventListener('input', function(){
+    disableUpdate = true;
+    var val = document.getElementById('progress').value;
+  })
